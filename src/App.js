6fb22a9615f0217eece917b110eea01c601a7386
unknown <davidnguyen21@gmail.com>
2022-06-29 00:39:16 -0700

    linting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ParentWrapper } from "./components/views";
+import { PlaceProvider, QueryProvider } from "./contexts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PlaceProvider>
+        <QueryProvider>
+          <ParentWrapper />
+        </QueryProvider>
+      </PlaceProvider>
     </div>
   );
 }
--- src/hooks/useAirPollution.js
+++ src/hooks/useAirPollution.js
@@ -5,7 +5,6 @@ import { PlaceContext } from "../contexts/PlaceContext";
 
 export const useAirPollution = () => {
   const { place } = useContext(PlaceContext);
-
   const queryString = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${place?.lat}&lon=${place?.lng}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
 
   const { isLoading, error, data } = useQuery(
