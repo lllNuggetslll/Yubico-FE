f30e9175d742e5f29472289c0835e38c620cce99
unknown <davidnguyen21@gmail.com>
2022-06-30 00:37:46 -0700

    update query strings, add units

--- src/hooks/useFiveDayWeather.js
+++ src/hooks/useFiveDayWeather.js
@@ -2,13 +2,18 @@ import { useContext } from "react";
 import { useQuery } from "react-query";
 
 import { PlaceContext } from "../contexts/PlaceContext";
+import { UnitContext } from "../contexts/unitContext";
+
+const MAX_DAYS = 5;
 
 export const useFiveDayWeather = () => {
   const { place } = useContext(PlaceContext);
-  const queryString = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${place?.lat}&lon=${place?.lng}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+  const { unit } = useContext(UnitContext);
+
+  const queryString = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${place?.lat}&lon=${place?.lng}&cnt=${MAX_DAYS}&units=${unit}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
 
   const { isLoading, error, data } = useQuery(
-    ["fiveDayWeather", place?.lat, place?.lng],
+    ["fiveDayWeather", place?.lat, place?.lng, unit],
     () => fetch(queryString).then((res) => res.json()),
     {
       enabled: !!place,
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -2,13 +2,15 @@ import { useContext } from "react";
 import { useQuery } from "react-query";
 
 import { PlaceContext } from "../contexts/PlaceContext";
+import { UnitContext } from "../contexts/unitContext";
 
 export const useWeather = () => {
   const { place } = useContext(PlaceContext);
-  const queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${place?.lat}&lon=${place?.lng}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+  const { unit } = useContext(UnitContext);
+  const queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${place?.lat}&lon=${place?.lng}&units=${unit}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
 
   const { isLoading, error, data } = useQuery(
-    ["todaysWeather", place?.lat, place?.lng],
+    ["todaysWeather", place?.lat, place?.lng, unit],
     () => fetch(queryString).then((res) => res.json()),
     {
       enabled: !!place,
