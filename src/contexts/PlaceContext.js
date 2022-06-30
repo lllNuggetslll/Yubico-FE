0b281b4476a81ca7360bb0aea9dbaf5cdcf40d4a
unknown <davidnguyen21@gmail.com>
2022-06-29 23:21:12 -0700

    add get local place func

--- src/contexts/PlaceContext.js
+++ src/contexts/PlaceContext.js
@@ -3,12 +3,21 @@ import React, { useState, createContext } from "react";
 export const PlaceContext = createContext(null);
 
 export const PlaceProvider = ({ children }) => {
-  const [place, setPlace] = useState(null);
+  const [place, setPlace] = useState({ lat: 37.3861, lng: -122.0839 }); // Mountain View
+
+  const getLocalPlace = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      selectPlace({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  };
 
   const selectPlace = (selectedPlace) => setPlace(selectedPlace);
 
   return (
-    <PlaceContext.Provider value={{ place, selectPlace }}>
+    <PlaceContext.Provider value={{ place, selectPlace, getLocalPlace }}>
       {children}
     </PlaceContext.Provider>
   );
