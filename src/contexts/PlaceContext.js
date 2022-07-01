3a4a267ac125899fd957bc6e842146d61ac759c8
unknown <davidnguyen21@gmail.com>
2022-07-01 01:48:07 -0700

    add default result fields

--- src/contexts/PlaceContext.js
+++ src/contexts/PlaceContext.js
@@ -3,11 +3,16 @@ import React, { useState, createContext } from "react";
 export const PlaceContext = createContext(null);
 
 export const PlaceProvider = ({ children }) => {
-  const [place, setPlace] = useState({ lat: 37.3861, lng: -122.0839 }); // Mountain View
+  const [place, setPlace] = useState({
+    result: "Mountain View, California, United States",
+    lat: 37.3861,
+    lng: -122.0839,
+  });
 
   const getLocalPlace = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       selectPlace({
+        result: "Local",
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
